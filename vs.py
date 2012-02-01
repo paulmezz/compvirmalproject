e90d109513d841f47c8253aaa532dc254229d8f3
Paul Mezzanin <paul@themezz.com>
2012-01-31 20:59:48 -0500

    wrapped up the loops

--- vs.py
+++ vs.py
@@ -6,12 +6,23 @@ import sys
 target_dir = os.walk(sys.argv[1])
 sig_file = sys.argv[2]
 
+#Open the file sig_file as a binary read only
+#then read in every line throwing them into the list
+#called lines
 with open(sig_file,'rb') as signatures:
-	lines = signatures.readlines()
-	for singlesig in lines:
-		print "imported: " + singlesig
+	signaturelines = signatures.readlines()
+#Just a debug to make sure it stepped through the lines
+#	for singlesig in lines:
+#		print "imported: " + singlesig
 
-
+#Step through the given directory
 for base_dir, sub_dirs, files in target_dir:
 	for filename in files:
-		print base_dir + "/" + filename
+#Right now this is a print, in the future it will scan the file
+#		print base_dir + "/" + filename
+		with open("./" + base_dir + "/" + filename,'rb') as activescan:
+			for scanline in activescan.readlines():
+				for singlesig in signaturelines:
+					print "singlesig = " + singlesig
+					if singlesig in scanline:
+						print singlesig + " found in " + filename
